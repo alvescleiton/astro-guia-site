791762ba509145a02f1b6784e53eea7162bad1eb
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-01-26 16:48:44 -0300

    home with banner and pods spaces

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,18 @@ import React from 'react'
 import ContainerPage from '@/components/layout/ContainerPage'
 
 const Home = () => {
-  return <ContainerPage>Home</ContainerPage>
+  return (
+    <ContainerPage>
+      <div className="flex h-36 w-full items-center justify-center bg-slate-800">Banner</div>
+
+      <div className="mt-4 grid grid-cols-2 gap-3">
+        <div className="min-h-16 rounded-lg bg-red-300"></div>
+        <div className="min-h-16 rounded-lg bg-blue-300"></div>
+        <div className="min-h-16 rounded-lg bg-green-300"></div>
+        <div className="min-h-16 rounded-lg bg-purple-300"></div>
+      </div>
+    </ContainerPage>
+  )
 }
 
 export default Home
