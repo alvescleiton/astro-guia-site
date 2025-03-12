b00d8ae6cdb00ab04c32e6da078a05c020c1ab1f
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-12 20:58:00 -0300

    remove suspense and lazy from react and using dynamic from next.js

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,50 +1,56 @@
 import Link from 'next/link'
-import React, { Suspense, lazy } from 'react'
+import dynamic from 'next/dynamic'
+import React from 'react'
 
-const FaHome = lazy(() => import('react-icons/fa').then((module) => ({ default: module.FaHome })))
-const FaRegNewspaper = lazy(() => import('react-icons/fa').then((module) => ({ default: module.FaRegNewspaper })))
-const GiNorthStarShuriken = lazy(() =>
-  import('react-icons/gi').then((module) => ({ default: module.GiNorthStarShuriken })),
+const FaHome = dynamic(() => import('react-icons/fa').then((module) => ({ default: module.FaHome })), { ssr: false })
+const FaRegNewspaper = dynamic(() => import('react-icons/fa').then((module) => ({ default: module.FaRegNewspaper })), {
+  ssr: false,
+})
+const GiNorthStarShuriken = dynamic(
+  () => import('react-icons/gi').then((module) => ({ default: module.GiNorthStarShuriken })),
+  { ssr: false },
 )
-const GiNightSleep = lazy(() => import('react-icons/gi').then((module) => ({ default: module.GiNightSleep })))
-const TbMapHeart = lazy(() => import('react-icons/tb').then((module) => ({ default: module.TbMapHeart })))
+const GiNightSleep = dynamic(() => import('react-icons/gi').then((module) => ({ default: module.GiNightSleep })), {
+  ssr: false,
+})
+const TbMapHeart = dynamic(() => import('react-icons/tb').then((module) => ({ default: module.TbMapHeart })), {
+  ssr: false,
+})
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full bg-background">
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="mx-auto flex min-h-20 max-w-[400px] items-center justify-evenly pb-1">
-          <div className="rounded-full bg-slate-800 p-3">
-            <Link href="/" aria-label="Home">
-              <FaHome size={20} aria-hidden="true" />
-            </Link>
-          </div>
-
-          <div className="rounded-full bg-slate-900 p-3">
-            <Link href="/horoscopo" prefetch={false} aria-label="Horóscopo">
-              <GiNorthStarShuriken size={20} aria-hidden="true" />
-            </Link>
-          </div>
-
-          <div className="rounded-full bg-slate-900 p-3">
-            <Link href="/sonhos" prefetch={false} aria-label="Sonhos">
-              <GiNightSleep size={20} aria-hidden="true" />
-            </Link>
-          </div>
-
-          <div className="rounded-full bg-slate-900 p-3">
-            <Link href="/noticias" prefetch={false} aria-label="Notícias">
-              <FaRegNewspaper size={20} aria-hidden="true" />
-            </Link>
-          </div>
-
-          <div className="rounded-full bg-slate-900 p-3">
-            <Link href="/mapa-astral" prefetch={false} aria-label="Mapa Astral">
-              <TbMapHeart size={20} aria-hidden="true" />
-            </Link>
-          </div>
+      <div className="mx-auto flex min-h-20 max-w-[400px] items-center justify-evenly pb-1">
+        <div className="rounded-full bg-slate-800 p-3">
+          <Link href="/" aria-label="Home">
+            <FaHome size={20} aria-hidden="true" />
+          </Link>
         </div>
-      </Suspense>
+
+        <div className="rounded-full bg-slate-900 p-3">
+          <Link href="/horoscopo" prefetch={false} aria-label="Horóscopo">
+            <GiNorthStarShuriken size={20} aria-hidden="true" />
+          </Link>
+        </div>
+
+        <div className="rounded-full bg-slate-900 p-3">
+          <Link href="/sonhos" prefetch={false} aria-label="Sonhos">
+            <GiNightSleep size={20} aria-hidden="true" />
+          </Link>
+        </div>
+
+        <div className="rounded-full bg-slate-900 p-3">
+          <Link href="/noticias" prefetch={false} aria-label="Notícias">
+            <FaRegNewspaper size={20} aria-hidden="true" />
+          </Link>
+        </div>
+
+        <div className="rounded-full bg-slate-900 p-3">
+          <Link href="/mapa-astral" prefetch={false} aria-label="Mapa Astral">
+            <TbMapHeart size={20} aria-hidden="true" />
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
