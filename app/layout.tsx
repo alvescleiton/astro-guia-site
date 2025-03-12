c15d8d35701b9343af113172ad79c16f956eb11c
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-12 20:53:56 -0300

    fix meta og and twitter infos and removing google analytics commented code

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,39 +36,18 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Astro Guia | O maior portal esotérico do Brasil" />
         <meta property="og:site_name" content="Astro Guia" />
-        <meta property="og:image" content={process.env.NEXT_PUBLIC_DOMAIN} />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/logo.webp`} />
         <meta property="og:image:alt" content="Astro Guia | O maior portal esotérico do Brasil" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:title" content="Astro Guia | O maior portal esotérico do Brasil" />
         <meta property="twitter:description" content="Astro Guia | O maior portal esotérico do Brasil" />
-        <meta property="twitter:image" content={process.env.NEXT_PUBLIC_DOMAIN} />
-        <meta property="og:image:alt" content="Astro Guia | O maior portal esotérico do Brasil" />
+        <meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/logo.webp`} />
+        <meta property="twitter:image:alt" content="Astro Guia | O maior portal esotérico do Brasil" />
 
         <link rel="icon" type="image/x-icon" href="/favicon/favicon-32x32.ico" />
-
         <link rel="apple-touch-icon" sizes="32x32" href="/favicon/favicon-32x32.ico" />
-
         <link rel="manifest" href="/manifest.json" />
-
-        {/* {process?.env?.NODE_ENV === 'production' && (
-          <>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EGZN7BHYEL" />
-            <Script
-              id="google-analytics"
-              strategy="beforeInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-
-                  gtag('config', 'G-EGZN7BHYEL');
-                `,
-              }}
-            />
-          </>
-        )} */}
       </head>
       <body className={`${font.className} antialiased`}>
         <DynamicHeader />
