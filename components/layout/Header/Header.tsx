fb9f45a30e9ce985c4b58fff96d2ca705113815f
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-12 21:05:12 -0300

    add type inference

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
 import Logo from '@/components/layout/Header/components/Logo'
-const Header = () => {
+
+const Header: React.FC = () => {
   return (
     <header className="fixed w-full bg-background">
       <div className="container mx-auto flex min-h-14 items-center justify-center md:justify-between">
