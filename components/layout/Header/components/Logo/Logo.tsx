032e3e98c1c96ef4c3d97177949843d5724b5de6
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-12 21:02:44 -0300

    add type on function and aria-lable at logo

--- components/layout/Header/components/Logo/Logo.tsx
+++ components/layout/Header/components/Logo/Logo.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
 import Link from 'next/link'
 import Image from 'next/image'
 
-const Logo = () => {
+const Logo: React.FC = () => {
   return (
-    <Link href="/" prefetch={false}>
+    <Link href="/" prefetch={false} aria-label="Astro Guia Home">
       <Image priority src="/assets/logo.webp" alt="Astro Guia" width={120} height={33} />
     </Link>
   )
