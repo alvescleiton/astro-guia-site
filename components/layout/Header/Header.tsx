421302d02000d01c753b2584f17637c650c85465
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-23 22:20:24 -0300

    fix header position

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '@/components/layout/Header/components/Logo'
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed w-full bg-background">
+    <header className="fixed top-0 z-50 w-full bg-background">
       <div className="container mx-auto flex min-h-14 items-center justify-center md:justify-between">
         <Logo />
       </div>
