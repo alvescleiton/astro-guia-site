0108c629214bf35b49eb49fcd4afbc4e304630d3
Cleiton Alves <cleiton_jc@yahoo.com.br>
2025-03-26 22:14:18 -0300

    fix header layout on mobile and desktop

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -4,7 +4,7 @@ import Logo from '@/components/layout/Header/components/Logo'
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 z-50 w-full bg-background">
-      <div className="container mx-auto flex min-h-14 items-center justify-center md:justify-between">
+      <div className="container mx-auto flex min-h-14 items-center">
         <Logo />
       </div>
     </header>
